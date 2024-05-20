5b01210cc0d6a3f996c77634f7dec206c3c01553
Rethabile Diale <rethadiale27@gmail.com>
2024-05-20 21:36:44 +0200

    Added thre 'reducer' function to take the current state and an action and return a new state

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ const decrement = () => ({ type: DECREMENT });
 const reset = () => ({ type: RESET });
 const add = () => ({ type: ADD });
 const subtract = () => ({ type: SUBTRACT });
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case RESET:
+      return { ...state, count: state.count + 1 };
+    case ADD:
+      return { ...state, count: state.count + 1 };
+    case SUBTRACT:
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
